7bc0a75e907d946c9b3c8998a0dbba399a02e693
Jo Carpenter <jordannc@stud.ntnu.no>
2021-06-04 11:36:05 -0400

    speed map

--- speed_map.m
+++ speed_map.m
@@ -1,10 +1,10 @@
 function [speed_grid,speedVec,speed] = speed_map(posx,posy,nbins)
 
 %compute velocity
-sampleRate = 50;
+sampleRate = 50; % in Hz
 velx = diff([posx(1); posx]); vely = diff([posy(1); posy]); % add the extra just to make the vectors the same size
 speed = sqrt(velx.^2+vely.^2)*sampleRate; 
-maxSpeed = 50; speed(speed>maxSpeed) = maxSpeed; %send everything over 50 cm/s to 50 cm/s
+maxSpeed = 70; speed(speed>maxSpeed) = maxSpeed; %send everything over 70 cm/s to 70 cm/s
 
 speedVec = maxSpeed/nbins/2:maxSpeed/nbins:maxSpeed-maxSpeed/nbins/2;
 speed_grid = zeros(numel(posx),numel(speedVec));
